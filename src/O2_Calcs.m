d647c5853f449a8679a7a078db87c73d2c5c45dd
F. Dylan Titmuss <51418386+FDylanT@users.noreply.github.com>
2022-10-03 18:37:43 -0400

    Update O2_Calcs.m

--- src/O2_Calcs.m
+++ src/O2_Calcs.m
@@ -1,7 +1,7 @@
 % Load data
-oxy = readtable("oxy_data.csv");
+oxy = readtable("data/oxy_data.csv");
 
 % Calc O2 solubility
 oxy.O2sol = gsw_O2sol(oxy.V1, oxy.V2, oxy.V3, oxy.V4, oxy.V5);
 
-writetable(oxy, "oxy_data.csv");
+writetable(oxy, "data/oxy_data.csv");
