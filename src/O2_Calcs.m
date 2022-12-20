d1a9f08d869a6434e3c3d84f7db64c8114d67a83
F. Dylan Titmuss <51418386+FDylanT@users.noreply.github.com>
2022-12-20 14:42:43 -0500

    push all changes

--- src/O2_Calcs.m
+++
@@ -1,7 +0,0 @@
-% Load data
-oxy = readtable("data/oxy_data.csv");
-
-% Calc O2 solubility
-oxy.O2sol = gsw_O2sol(oxy.V1, oxy.V2, oxy.V3, oxy.V4, oxy.V5);
-
-writetable(oxy, "data/oxy_data.csv");
---
+++ src/mixedLayer.m
@@ -0,0 +1,42 @@
+cd Repos/ScallopRSA2021
+
+%% Load October rsk files
+
+oct1 = RSKopen('data/CTD/RSK_raw/RSA_20211006_021.rsk');
+oct1 = RSKreaddata(oct1, 't1', datenum(2021, 10, 05), 't2', datenum(2021, 10, 10));
+
+oct2 = RSKopen('data/CTD/RSK_raw/RSA_20211008_064.rsk');
+oct2 = RSKreaddata(oct2, 't1', datenum(2021, 10, 05), 't2', datenum(2021, 10, 10));
+
+oct3 = RSKopen('data/CTD/RSK_raw/RSA_20211010_114.rsk');
+oct3 = RSKreaddata(oct3, 't1', datenum(2021, 10, 05), 't2', datenum(2021, 10, 10));
+
+% read downcasts from all profiles
+oct1down = RSKreadprofiles(oct1, 'direction', 'down'); % 22 profiles
+oct2down = RSKreadprofiles(oct2, 'direction', 'down'); % 47 profiles
+oct2up = RSKreadprofiles(oct2, 'direction', 'up'); % upcast of profile 12
+oct3down = RSKreadprofiles(oct3, 'direction', 'down'); % 50 profiles
+
+% derive sea pressure
+oct1down = RSKderiveseapressure(oct1down); % 10.1325 patm
+oct2down = RSKderiveseapressure(oct2down); % 10.1325 patm
+oct2up = RSKderiveseapressure(oct2up);
+oct3down = RSKderiveseapressure(oct3down); % 10.1325 patm
+
+% derive depth
+oct1down = RSKderivedepth(oct1down);
+oct2down = RSKderivedepth(oct2down);
+oct2up = RSKderivedepth(oct2up);
+oct3down = RSKderivedepth(oct3down);
+
+%% Calculate mixed-layer depths
+
+% Levitus 1982:
+% depth at which temp change from surface is 0.5°C
+% depth at which sigmaT change from surface sigma-t is 0.125
+    % sigmaT = rho(S,T) - 1000 kg/m^3
+        % density 1.027 g/cm3 = sigmaT 27 kg/m3
+
+oct1down.data.values[?].sigmaT = NaN;
+oct1down.data.values[?].sigmaT = (density - 1) * 1000;
+
